e7968b3226792cff567ef0eda6276344dd842bad
husniAT <husni73890@gmail.com>
2023-05-02 10:08:25 +0300

    first commit

---
+++ android/app/src/main/java/com/example/face_and_authentication/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.face_and_authentication;
+
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+public class MainActivity extends FlutterFragmentActivity {
+}
---
+++ lib/services/local_auth_service.dart
@@ -0,0 +1,48 @@
+import 'dart:core';
+
+import 'package:flutter/foundation.dart';
+import 'package:local_auth/local_auth.dart';
+
+class LocalAuth{
+  static final _auth=LocalAuthentication();
+  static Future<bool>_canAuthenticate()async =>await _auth.canCheckBiometrics || await _auth.isDeviceSupported();
+
+
+  static Future<bool> authenticate() async {
+    try{
+      if(!await _canAuthenticate()) return false;
+      return await _auth.authenticate(
+        authMessages: const [
+
+
+
+
+        ],
+
+
+          localizedReason: "Use Face Id to authenticate",
+           options: const AuthenticationOptions(
+             useErrorDialogs: true,
+             stickyAuth:true
+           ),
+
+
+
+      );
+
+
+
+    }
+    catch(e) {
+      debugPrint("error $e");
+      return false;
+
+
+    }
+
+
+
+
+  }
+
+}
